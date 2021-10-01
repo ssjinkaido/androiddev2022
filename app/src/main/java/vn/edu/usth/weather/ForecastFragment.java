486460a45301b2fe549a0dc3791fab70a01928dd
ssjinkaido <tungxuannguyen16@gmail.com>
2021-10-01 08:55:24 +0700

    Update labwork 2

---
+++ app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -0,0 +1,30 @@
+package vn.edu.usth.weather;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ForecastFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ForecastFragment extends Fragment {
+
+    public static final String LOG_TAG = "";
+
+    public ForecastFragment() {
+        //Empty
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_forecast, container, false);
+    }
+}
